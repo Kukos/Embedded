94d8c1f32538ce3782d173afc0758ca0a9ff417c
Michal Kukowski <michalkukowski10@gmail.com>
2017-03-23 13:41:01 +0100

    L3

--- L3/src/lfsr.vhdl
+++ L3/src/lfsr.vhdl
@@ -2,21 +2,20 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
 
-
 entity lfsr is
-    Port ( clk : in  STD_LOGIC;
-           q : inout  STD_LOGIC_VECTOR(15 downto 0) := (OTHERS => '0')
+    port (
+            clk     :in     std_logic;
+            q       :inout  std_logic_vector(15 downto 0) := (others => '0')
 			);
 end lfsr;
 
-ARCHITECTURE Behavioral OF lfsr IS
-BEGIN
-  PROCESS
-  BEGIN
-	q(15 downto 1) <= q(14 downto 0);
-	q(0) <= not(q(15) XOR q(14) XOR q(13) XOR q(4));
-	
-	WAIT UNTIL clk'event AND clk='1';
-  END PROCESS;
-END Behavioral;
+architecture Behavioral of lfsr is
+begin
+    process
+    begin
+	    q(15 downto 1) <= q(14 downto 0);
+	    q(0) <= not(q(15) xor q(14) xor q(13) xor q(4));
 
+	    wait until clk'event and clk = '1';
+    end process;
+end Behavioral;
